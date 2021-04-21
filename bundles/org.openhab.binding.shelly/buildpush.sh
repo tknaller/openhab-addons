6e561e91757abd78054a40fbe184588a1b3733f2
Markus Michels <markus7017@gmail.com>
2021-04-21 15:41:27 +0200

    fix
    Signed-off-by: Markus Michels <markus7017@gmail.com>

--- bundles/org.openhab.binding.shelly/buildpush.sh
+++ bundles/org.openhab.binding.shelly/buildpush.sh
@@ -6,27 +6,28 @@ then
 fi
 
 echo Build Version 2
-mvn -Dohc.version=3.0.1 clean install
+mvn clean install
 if [ $? -ne 0 ]
 then
 	echo Build version 2 failed
 	exit
 fi
-#mvn -Dohc.version=3.0.1 clean install -pl :org.openhab.binding.shelly karaf:kar clean install
+#mvn install -pl :org.openhab.binding.shelly karaf:kar clean install
 cp target/org.openhab.binding.shelly-2.5.*-SNAPSHOT.jar ~/Dev/myfiles/shelly/
 #cp target/org.openhab.binding.shelly-2.5.*-SNAPSHOT.kar ~/Dev/myfiles/shelly/
 
 echo Copy code to V3, update Shelly from v2 to v3
 ./copy3.sh
 echo Build Version 3
+cd ~/Dev/openhab-3/git/openhab-addons/bundles/org.openhab.binding.shelly
 #mvn clean install
-mvn -Dohc.version=3.0.1 clean install
+mvn clean install -Dohc.version=3.0.1
 if [ $? -ne 0 ]
 then
 	echo Build version 3 failed
 	exit
 fi
-#mvn -Dohc.version=3.0.1 clean install -pl :org.openhab.binding.shelly karaf:kar
+#mvn -Dohc.version=3.0.1 clean install -Dohc.version=3.0.1 -pl :org.openhab.binding.shelly karaf:kar
 cp ~/Dev/openhab-3/git/openhab-addons/bundles/org.openhab.binding.shelly/target/org.openhab.binding.shelly-3.*-SNAPSHOT.jar ~/Dev/myfiles/shelly/
 #cp ~/Dev/openhab-3/git/openhab-addons/bundles/org.openhab.binding.shelly/target/org.openhab.binding.shelly-3.*-SNAPSHOT.kar ~/Dev/myfiles/shelly/
 
